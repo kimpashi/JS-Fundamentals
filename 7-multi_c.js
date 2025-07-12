453970ede2e3f0bbd694ef553cd12bbd198a57d2
kimpashi <marouanebousselham@gmail.com>
2025-07-12 19:25:06 +0100

    Fix script to print 'C is fun' x times only for positive integers

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const arg = process.argv[2];
 const x = parseInt(arg);
 
-if (!arg || isNaN(x) || x < 1) {
+if (!arg || isNaN(x)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (x > 0) {
   let i = 0;
   while (i < x) {
     console.log("C is fun");
